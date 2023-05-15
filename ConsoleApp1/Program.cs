96a0541651422841d8efffcbbea111dd3645d7fb
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-05-15 14:24:07 +0800

    C# 的 資料型態 / 變數

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-System.Console.WriteLine('全');
+//字串 string "全權"
+//字元 char 'a'
+//整數 int 40
+//浮點 double 160.5
+//布林 bool true false
+
+System.Console.WriteLine("全權");
+System.Console.WriteLine('a');
+System.Console.WriteLine(160.5);
+System.Console.WriteLine(true);
+
+//變數
+string name = "全權";
+char sex = 'M';
+int age = 30;
+double height = 180.3;
+bool is_male = true;
+/* 資料型態不能改變... */
+
+
+System.Console.WriteLine("有個人名叫"+name);
+name = "小黃";
+System.Console.WriteLine("改名叫" + name);
+System.Console.WriteLine(sex);
+System.Console.WriteLine(height);
+System.Console.WriteLine(is_male);
+
+
+
