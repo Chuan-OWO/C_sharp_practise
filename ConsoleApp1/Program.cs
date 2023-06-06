031b5239290cff1f419a16d88da40380ef6f8ce6
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-06 18:42:46 +0800

    for

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-System.Console.WriteLine('全');
+﻿/*
+for (int i = 0; i < 5; i++)
+{   //01234
+    Console.Write(i);
+   
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+//方形/長方
+int input = int.Parse(Console.ReadLine());
+for (int i = 0; i < input; i++)
+{
+    for(int y = 0; y <= input ; y++)
+    {    
+            Console.Write('*');
+    }
+    Console.WriteLine();
+}
+Console.Read();
+*/
+/*
+//正直角三角形
+int input1 = int.Parse(Console.ReadLine());
+for (int i = 0; i < input1; i++)
+{
+    for (int y = 0; y <= input1; y++)
+    {
+        if (i > y)
+        {
+            Console.Write('*');
+        }
+    }
+    Console.WriteLine();
+}
+Console.Read();
+*/
+
+/*
+int input2 = int.Parse(Console.ReadLine());
+for (int i = 0; i < input2; i++)
+{
+    for (int y = 0; y <= input2-1; y++)
+    {
+        if (i > y)
+        {
+            Console.Write('*');
+        }
+    }
+    Console.WriteLine();
+}
+Console.Read();
+*/
+
+
