f7de99c7bbd2c4db83670348738ddc31b9c8bed8
chuan <101337985+Chuan-OWO@users.noreply.github.com>
2023-06-06 16:38:26 +0800

    Console.ReadLine();

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-System.Console.WriteLine('全');
+//取得用戶輸入
+Console.Write("請輸入你的姓名:");
+string a = Console.ReadLine();
+
+Console.WriteLine("@@@"+ a);
+/*
+    Console.ReadLine(); 
+    可以取得用戶輸入的值，
+    將取得的值，傳給一個變數
+    再用 Console.WriteLine 變數顯示變數存放的資料
+*/
